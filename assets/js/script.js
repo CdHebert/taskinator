ee415ea3129ff05e175f4f9b37e137cdc1f8bb45
CBear <chebert222@gmail.com>
2021-03-31 17:20:40 -0700

    added if return false and reset function

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,7 +6,11 @@ var taskFormHandler = function(event) {
     event.preventDefault();
     var taskNameInput = document.querySelector("input[name='task-name']").value;
     var taskTypeInput = document.querySelector("select[name='task-type']").value;
-
+        if (!taskNameInput || !taskTypeInput) {
+            alert("You need to fill out the task form!");
+            return false;
+        }
+        formEl.reset();
     //package up data as an object
     var taskDataObj = {
         name: taskNameInput,
@@ -26,11 +30,10 @@ listItemEl.className = "task-item";
 var taskInfoEl = document.createElement("div");
 taskInfoEl.className = "task-info";
 taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskDataObj.name + "</h3><span class='task-type'>" + taskDataObj.type + "</span>";
-
 listItemEl.appendChild(taskInfoEl);
 
 // add entire list item to list
 tasksToDoEl.appendChild(listItemEl);
-}
+};
 
 formEl.addEventListener("submit", taskFormHandler);
