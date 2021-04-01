0f093fe19200a1b68c08fcda1b5010061e6e4e6b
CBear <chebert222@gmail.com>
2021-04-01 15:35:58 -0700

    finished edit function

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,6 +3,18 @@ var tasksToDoEl = document.querySelector("#tasks-to-do");
 var taskIdCounter = 0;
 var pageContentEl =  document.querySelector("#page-content");
 
+var completeEditTask = function(taskName, taskType, taskId) {
+    var taskSelected = document.querySelector(".task-item[data-task-id='" + taskId + "']");
+
+// set new values
+taskSelected.querySelector("h3.task-name").textContent = taskName;
+taskSelected.querySelector("span.task-type").textContent = taskType;
+
+alert("Task Updated!");
+formEl.removeAttribute("data-task-id");
+document.querySelector("#save-task").textContent = "Add Task";
+};
+
 
 var taskFormHandler = function(event) {
     event.preventDefault();
@@ -13,14 +25,22 @@ var taskFormHandler = function(event) {
             return false;
         }
         formEl.reset();
-    //package up data as an object
-    var taskDataObj = {
-        name: taskNameInput,
-        type: taskTypeInput
-    };
 
-    // send it as an argument to createTaskEl
+    var isEdit = formEl.hasAttribute("data-task-id");
+    // has data attribute, so get task id and call function to complete edit process
+if (isEdit) {
+    var taskId = formEl.getAttribute("data-task-id");
+    completeEditTask(taskNameInput, taskTypeInput, taskId);
+  } 
+  // no data attribute, so create object as normal and pass to createTaskEl function
+  else {
+    var taskDataObj = {
+      name: taskNameInput,
+      type: taskTypeInput
+    };
+  
     createTaskEl(taskDataObj);
+  }
 };
 
 var createTaskEl = function(taskDataObj) {
